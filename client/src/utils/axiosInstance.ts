52150fbfc83f14da46b6f33f8f74d7cd6fad35cb
Oops61rus <antodeni@rambler.ru>
2020-12-01 22:35:07 +0300

    add axios

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseUrl: "http://localhost:4000/api/v1",  // убрать в переменную
-  
+  baseURL: "http://localhost:4000/api/v1",  // убрать в переменную
 });
 
 export default axiosInstance;
