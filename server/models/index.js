a490fb8095daf0132b25a11da46fa2fb5b9580b5
Oops61rus <antodeni@rambler.ru>
2020-12-01 22:36:54 +0300

    fix models

--- server/models/index.js
+++ server/models/index.js
@@ -1,18 +1,18 @@
-const config = require("../config/config");
-const Sequelize = require("sequelize");
+const config = require('../config/config');
+const Sequelize = require('sequelize');
 const sequelize = new Sequelize(...config);
 
-const User = require("./user");
-
-sequelize
-  .sync()
-  .then(() => console.log("Databases created!"))
-  .catch((err) => console.log(err));
+const User = require('./user');
 
 sequelize
   .authenticate()
-  .then(() => console.log("Connection has been established successfully!"))
+  .then(() => console.log('Connection has been established successfully!'))
   .catch((err) => console.log("Unable to connect to database", err));
+
+sequelize
+  .sync()
+  .then(() => console.log('Databases created!'))
+  .catch((err) => console.log(err));
 
 module.exports = {
   User,
