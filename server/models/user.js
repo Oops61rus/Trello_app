dc952716201d647a1ff1eedfdb955fee31c3eb1a
Oops61rus <antodeni@rambler.ru>
2020-12-05 14:18:44 +0300

    fix models, add code for add all models

--- server/models/user.js
+++ server/models/user.js
@@ -2,9 +2,19 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     "User",
     {
-      name: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
     {
       underscored: true,
